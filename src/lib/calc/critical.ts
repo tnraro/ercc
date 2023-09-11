2b875e2ce47dc3a461990299c4f374d4cca14f93
tnraro <deokji53@gmail.com>
2023-09-11 21:26:47 +0900

    fix: correct critical damage base

--- src/lib/calc/critical.ts
+++ src/lib/calc/critical.ts
@@ -1,4 +1,4 @@
-export const calcCritical = (atk: number, cc = 0, cd = 0, cdb = 1.65): number => {
+export const calcCritical = (atk: number, cc = 0, cd = 0, cdb = 1.60): number => {
   const _cc = Math.max(Math.min(cc, 1), 0);
   return Math.floor(atk * (1 - _cc) + atk * _cc * (cdb + cd));
 }
