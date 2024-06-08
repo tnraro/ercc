98ca44ea773d1c73417ef9b3856554bd48eaf396
tnraro <deokji53@gmail.com>
2024-06-08 14:08:11 +0900

    add test config

---
+++ setupTest.ts
@@ -0,0 +1,5 @@
+import matchers from "@testing-library/jest-dom/matchers";
+import { expect, vi } from "vitest";
+
+console.log(matchers)
+expect.extend(matchers);
--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
@@ -5,4 +6,9 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 export default defineConfig({
   plugins: [svelte()],
   base: "/ercc/",
+  test: {
+    globals: true,
+    environment: "happy-dom",
+    setupFiles: ["./setupTest.ts"],
+  }
 })
