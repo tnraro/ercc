6a38e6837def43352c867c40f542b3ade48db0fd
Hojin Yang <deokji53@gmail.com>
2024-06-16 21:38:26 +0900

    Feat/performance (#5)
    * add test for getCombinations()
    * reuse template string to avoid allocation
    
    This change reduces the getCombinations() execution time from 2.9s to 0.7s

--- src/features/BestCombinations.svelte
+++ src/features/BestCombinations.svelte
@@ -11,82 +11,8 @@
   import Pagination from "./pagination/Pagination.svelte";
   // consts
   const getCombinations = () => {
-    if (weapon == null) return;
-    if (character == null) return;
-
-    const filteredItems = items
-      .filter((item) => includeLegendaryItem || item.grade !== "legendary")
-      .filter((item) => includeMythicItem || item.grade !== "mythic");
-    const itemsBy = ["Chest", "Head", "Arm", "Leg"].reduce(
-      (acc, type) => ({
-        ...acc,
-        [type.toLowerCase()]: filteredItems
-          .filter((item) => item.type === type)
-          .filter(
-            (item) =>
-              excludedItems[type].size === 0 ||
-              !excludedItems[type].has(item.id),
-          )
-          .filter(
-            (item) =>
-              includedItems[type].size === 0 ||
-              includedItems[type].has(item.id),
-          ),
-      }),
-      {} as Record<string, ItemData[]>,
-    );
-
-    const weaponData = sw.find(
-      ({ id, weaponType }) => id === character.id && weaponType === weaponType,
-    )?.stats;
-
-    const base = {
-      as: dot("as", character, weaponTypeInfo.get(weaponType!)!),
-      asr: (weaponData?.asr ?? 0) * weaponLevel,
-      adm: (weaponData?.adm ?? 0) * weaponLevel,
-    };
-
-    let combs = [];
     console.time("combination");
-    for (const chest of itemsBy.chest) {
-      for (const head of itemsBy.head) {
-        for (const arm of itemsBy.arm) {
-          for (const leg of itemsBy.leg) {
-            const atk =
-              dot("atk", character, weapon, chest, head, arm, leg) -
-              character.atkLv;
-            const asr = dot("asr", weapon, chest, head, arm, leg) + base.asr;
-            const as = Math.max(
-              Math.min(base.as * (1 + asr), character.asl),
-              character.asm,
-            );
-            const adm = base.adm;
-            const cc = dot("cc", character, weapon, chest, head, arm, leg);
-            const cd = dot("cd", weapon, chest, head, arm, leg);
-            const pd = dot("pd", weapon, chest, head, arm, leg);
-            const pdr = dot("pdr", weapon, chest, head, arm, leg);
-            const critical = calcCritical((atk * (1 + adm)) | 0, cc, cd);
-            const damage = calcDamage(critical, pd, pdr, targetDefense);
-            combs.push({
-              equipments: [weapon.id, chest.id, head.id, arm.id, leg.id],
-              stats: {
-                dps: damage * as,
-                atk,
-                asr,
-                adm,
-                as,
-                cc,
-                cd,
-                pd,
-                pdr,
-                critical,
-                damage,
-              },
-            });
-          }
-        }
-      }
-    }
+    const combs = calcCombinations();
     results = combs.length;
     combs.sort((a, b) => b.stats.atk - a.stats.atk);
     combs.sort((a, b) => b.stats.dps - a.stats.dps);
@@ -95,17 +21,113 @@
     combinations = combs;
     console.timeEnd("combination");
 
-    function current<T extends string>(
-      id: T,
-      obj: { [key in T | `${T}Lv`]?: number },
-    ) {
-      return (obj[id] ?? 0) + (obj[`${id}Lv`] ?? 0) * characterLevel;
-    }
-    function dot<T extends string>(
-      id: T,
-      ...objs: { [key in T | `${T}Lv`]?: number }[]
-    ) {
-      return objs.reduce((acc, x) => acc + current(id, x), 0);
+    function calcCombinations() {
+      if (weapon == null || character == null || weaponType == null) return [];
+
+      const filteredItems = items
+        .filter((item) => includeLegendaryItem || item.grade !== "legendary")
+        .filter((item) => includeMythicItem || item.grade !== "mythic");
+
+      const itemsBy = ["Chest", "Head", "Arm", "Leg"].reduce(
+        (acc, type) => ({
+          ...acc,
+          [type.toLowerCase()]: filteredItems
+            .filter((item) => item.type === type)
+            .filter(
+              (item) =>
+                excludedItems[type].size === 0 ||
+                !excludedItems[type].has(item.id),
+            )
+            .filter(
+              (item) =>
+                includedItems[type].size === 0 ||
+                includedItems[type].has(item.id),
+            ),
+        }),
+        {} as Record<string, ItemData[]>,
+      );
+
+      const weaponData = sw.find(
+        ({ id, weaponType }) =>
+          id === character.id && weaponType === weaponType,
+      )!.stats;
+
+      const atk0 =
+        character.atk +
+        character.atkLv * characterLevel +
+        (weapon.atk + weapon.atkLv * characterLevel) -
+        character.atkLv;
+      const as0 =
+        character.as +
+        character.asLv * characterLevel +
+        weaponTypeInfo.get(weaponType)!.as;
+      const asr0 =
+        weapon.asr +
+        weapon.asrLv * characterLevel +
+        weaponData.asr * weaponLevel;
+      const adm0 = weaponData.adm * weaponLevel;
+      const cc0 = character.cc + character.ccLv * characterLevel + weapon.cc;
+      const cd0 = weapon.cd;
+      const pd0 = weapon.pd;
+      const pdr0 = weapon.pdr;
+
+      const result = [];
+      for (const chest of itemsBy.chest) {
+        const atk1 = atk0 + (chest.atk + chest.atkLv * characterLevel);
+        const asr1 = asr0 + (chest.asr + chest.asrLv * characterLevel);
+        const cc1 = cc0 + chest.cc;
+        const cd1 = cd0 + chest.cd;
+        const pd1 = pd0 + chest.pd;
+        const pdr1 = pdr0 + chest.pdr;
+        for (const head of itemsBy.head) {
+          const atk2 = atk1 + (head.atk + head.atkLv * characterLevel);
+          const asr2 = asr1 + (head.asr + head.asrLv * characterLevel);
+          const cc2 = cc1 + head.cc;
+          const cd2 = cd1 + head.cd;
+          const pd2 = pd1 + head.pd;
+          const pdr2 = pdr1 + head.pdr;
+          for (const arm of itemsBy.arm) {
+            const atk3 = atk2 + (arm.atk + arm.atkLv * characterLevel);
+            const asr3 = asr2 + (arm.asr + arm.asrLv * characterLevel);
+            const cc3 = cc2 + arm.cc;
+            const cd3 = cd2 + arm.cd;
+            const pd3 = pd2 + arm.pd;
+            const pdr3 = pdr2 + arm.pdr;
+            for (const leg of itemsBy.leg) {
+              const atk = atk3 + (leg.atk + leg.atkLv * characterLevel);
+              const asr = asr3 + (leg.asr + leg.asrLv * characterLevel);
+              const as = Math.max(
+                Math.min(as0 * (1 + asr), character.asl),
+                character.asm,
+              );
+              const adm = adm0;
+              const cc = cc3 + leg.cc;
+              const cd = cd3 + leg.cd;
+              const pd = pd3 + leg.pd;
+              const pdr = pdr3 + leg.pdr;
+              const critical = calcCritical((atk * (1 + adm)) | 0, cc, cd);
+              const damage = calcDamage(critical, pd, pdr, targetDefense);
+              result.push({
+                equipments: [weapon.id, chest.id, head.id, arm.id, leg.id],
+                stats: {
+                  dps: damage * as,
+                  atk,
+                  asr,
+                  adm,
+                  as,
+                  cc,
+                  cd,
+                  pd,
+                  pdr,
+                  critical,
+                  damage,
+                },
+              });
+            }
+          }
+        }
+      }
+      return result;
     }
   };
   function sortBy(stats: string) {
